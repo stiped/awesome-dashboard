bbf5cb5d2ef19843226906b10d5ec0b06ca26a75
Stian Pedersen <stian.pedersen@ciber.no>
2018-03-25 13:29:13 +0200

    Lagt til tinylog

--- src/main/java/awesome/dashboard/Webserver.java
+++ src/main/java/awesome/dashboard/Webserver.java
@@ -4,13 +4,24 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 import org.glassfish.jersey.servlet.ServletContainer;
+import org.pmw.tinylog.Configurator;
+import org.pmw.tinylog.Level;
+import org.pmw.tinylog.Logger;
+import org.pmw.tinylog.writers.ConsoleWriter;
+
 
 public class Webserver {
 
     public static void main(String[] args) throws Exception {
 
+        Configurator.currentConfig()
+                .level(Level.DEBUG)
+                .writer(new ConsoleWriter(), "{date:dd-MM-yyyy HH:mm:ss} {level}\t{class} - {message}")
+//                .addWriter(new FileWriter("log.txt"), "{class}.{method}()\t{message}")
+                .activate();
         PropertiesService.init();
 
+        Logger.debug("Starter webapp");
         Server server = new Server(8080);
         ServletContextHandler ctx = new ServletContextHandler(ServletContextHandler.NO_SESSIONS);
         ctx.setContextPath("/");
@@ -20,6 +31,7 @@ public class Webserver {
         servletHolder.setInitOrder(1);
         servletHolder.setInitParameter("jersey.config.server.provider.packages", "awesome.dashboard.api");
         server.start();
+        Logger.info("Awesome backend startet: http://localhost:{}", server.getURI().getPort());
         server.join();
     }
 }
